d845e9d58b26bd127c39b2669606616884ff0754
Bhupiiam <97390120+Bhupiiam@users.noreply.github.com>
2022-05-23 23:25:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+(1) Build a Multi-Page Responsive Website.
+(2) Redesign an Existing Website Template.
+(3) Design a Small JavaScript Game.
+(4) Create a Simple Application.
+(5) Random Number Generator.
+(6) Web Scraping Using Python.
+(7) Build a Working Chess Game.
+
+
+
+Hello, World!
+Variables and Types
+Arrays
+Multidimensional Arrays
+Conditions
+Strings
+For loops
+While loops
+Functions
+Static
+
+Pointers
+Structures
+Function arguments by reference
+Dynamic allocation
+Arrays and Pointers
+Recursion
+Linked lists
+Binary trees
+Unions
+Pointer Arithmetics
+Function Pointers
+Bitmasks
+
